3016d09036248f7ad9e53065f4795f56273ce2bd
Tran Manh Linh <devngmo@gmail.com>
2022-08-10 14:59:47 +0700

    use regex to check email format

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
-def isValidEmailAddress(text:str):
-    atIdx = text.index('@')
-    return atIdx > 0 and atIdx < len(text)
+import re   
+  
+regexEmailAddress = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' 
+
+def isValidEmailAddress(text):
+    return re.search(regexEmailAddress, text)
