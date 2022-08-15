ecfc22a837cb085061afe67e42ff1f91e0db6c2e
Tran Manh Linh <devngmo@gmail.com>
2022-08-15 16:05:43 +0700

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@cls
-
-set API_ENDPOINT_EMAIL_CONFIRM=http://127.0.0.1:8000
-set HOST_IP=127.0.0.1
-set HOST_PORT=8000
-set GMAIL_ACCOUNT=test@gmail.com
-set GMAIL_APP_PASSWORD=1231231231232133
-cd app
-python -m uvicorn main:app --host %HOST_IP% --port %HOST_PORT%
