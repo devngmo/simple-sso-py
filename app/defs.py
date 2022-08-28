9ecbf0ee54edce705e1648f39568734fc7b13af8
Tran Manh Linh <devngmo@gmail.com>
2022-08-28 23:07:58 +0700

    Update defs.py

--- app/defs.py
+++ app/defs.py
@@ -1,6 +1,32 @@
+from ctypes import util
+import os, utils
 ERRCODE_NONE = 'ERRCODE_NONE'
+ERRCODE_BAD_REQUEST = 'ERRCODE_BAD_REQUEST'
 ERRCODE_CONFIRM_LINK_EXPIRED = 'ERRCODE_CONFIRM_LINK_EXPIRED'
 ERRCODE_REGISTER_EMAIL_ALREADY_EXIST = 'ERRCODE_REGISTER_EMAIL_ALREADY_EXIST'
 ERRCODE_REGISTER_PHONE_ALREADY_EXIST = 'ERRCODE_REGISTER_PHONE_ALREADY_EXIST'
 
 ERRCODE_CLIENT_ALREADY_EXISTS = 'ERRCODE_CLIENT_ALREADY_EXISTS'
+
+
+TOKEN_TYPE_REGISTRATION_CONFIRM = 'registration-confirm-token'
+TOKEN_TYPE_AUTH_ACCESS = 'auth-access-token'
+
+
+tags_metadata = [
+    {
+        'name': 'accounts',
+        'description': 'Operations with Accounts: Register/ Login / Logout'
+    },
+    {
+        'name': 'token',
+        'description': 'Operations with Token: Create / verify'
+    },
+    {
+        'name': 'oauth',
+        'description': 'Operations with OAuth: SignIn'
+    }
+]
+
+DATA_FOLDER='data'
+JWT_PRIVATE_KEY = utils.loadText(os.path.join(DATA_FOLDER, 'jwt-key'))
