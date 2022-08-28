1ac43f2d0167b9ec43bc34bd68d2e812ea30b190
Tran Manh Linh <devngmo@gmail.com>
2022-08-28 23:11:06 +0700

    Update registration_validator.py

--- app/registration_validator.py
+++ app/registration_validator.py
@@ -1,36 +1,41 @@
 from re import S
+
 from s_mail import EmailService
+from s_log import LogService
 from repo_account import AccountRepository
 from repo_token import TokenRepository
 from models import account as ModelAccount
 import defs
 class ValidateConfirmCodeResult():
-    def __init__(self, errorCode):
-        self.errorCode = errorCode
+    def __init__(self, errCode):
+        self.errCode = errCode
 
 class RegistrationValidator():
-    def __init__(self, accRepo:AccountRepository, tokenRepo:TokenRepository, emailService: EmailService):
+    def __init__(self, logService: LogService, accRepo:AccountRepository, tokenRepo:TokenRepository, emailService: EmailService):
         self.accRepo = accRepo
         self.tokenRepo = tokenRepo
         self.emailService = emailService
+        self.logService = logService
 
     def validateConfirmCode(self, token):
-        print('[RegistrationValidator] validate confirm code: %s' % token)
-        metadata = self.tokenRepo.get(token)
+        metadata = self.tokenRepo.getToken(token)
         if metadata == None:
-            print('[RegistrationValidator] CONFIRM TOKEN not found')
-            return ValidateConfirmCodeResult(errorCode=defs.ERRCODE_CONFIRM_LINK_EXPIRED)
+            self.logService.error('RegistrationValidator', 'validateConfirmCode token=%s => Token not found' % (token))
+            return ValidateConfirmCodeResult(errCode=defs.ERRCODE_CONFIRM_LINK_EXPIRED)
 
-        accID = metadata['id']
-        client_id = metadata['client_id']
-        acc = self.accRepo.finByID(client_id, accID)
-        acc['activated'] = True
-        self.accRepo.updateAccount(client_id, acc)
-        print('[RegistrationValidator] Account %s was ACTIVATED' % acc['id'])
-        return ValidateConfirmCodeResult(errorCode=defs.ERRCODE_NONE)
-    
+        accID = metadata['account_id']
+        tokenType = metadata['type']
+        if tokenType == defs.TOKEN_TYPE_REGISTRATION_CONFIRM:
+            self.accRepo.updateAccount(accID, {'activated': True})
+            self.logService.debug('RegistrationValidator', 'validateConfirmCode token=%s => Token not found' % (token), state='success')
+            return ValidateConfirmCodeResult(errCode=defs.ERRCODE_NONE)
+        else:
+            self.logService.error('RegistrationValidator', 'validateConfirmCode token=%s => invalid token type: %s' % (token, tokenType))
+            return ValidateConfirmCodeResult(errCode=defs.ERRCODE_BAD_REQUEST)
+
     def sendValidateEmail(self, account : ModelAccount.Account, token, endpoint):
-        url = '{endpoint}/register/validate/{token}'.format(endpoint=endpoint, token=token)
+        print('send validate email to ', account.email)
+        url = '{endpoint}/api/v1/auth/registration/validate/{token}'.format(endpoint=endpoint, token=token)
         html = """
         <h1>SSO Registration Confirm</h1>
         <a href="{url}">{url}</a>
