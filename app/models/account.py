8c3dc9a31132326eeab65d59dd0efba5065ec1c2
Tran Manh Linh <devngmo@gmail.com>
2022-08-28 23:08:48 +0700

    implement Account.createPasshash

--- app/models/account.py
+++ app/models/account.py
@@ -1,8 +1,10 @@
 
-import json
+import json, os, defs
 from pydantic import BaseModel
+from passlib.hash import pbkdf2_sha256
+
+
 class RegistrationModel(BaseModel):
-    id: str
     email: str
     phone: str
     name: str
@@ -10,20 +12,24 @@ class RegistrationModel(BaseModel):
     address: str
 
 class Account():
-    def __init__(self, id, name, email, phone, address, passhash, activated):
-        self.id = id
+    def __init__(self, name, email, phone, address, passhash, activated, is_tenant, parent_tenant_id):
         self.name = name
         self.email = email
         self.phone = phone
         self.address =address
         self.passhash = passhash
         self.activated = activated
+        self.is_tenant = is_tenant
+        self.parent_tenant_id = parent_tenant_id
 
     def toJSON(self):
         return json.dumps(self, default=lambda o : o.__dict__, sort_keys=True, indent=4)
 
+    def toDict(self):
+        return self.__dict__
+
 def createPasshash(password):
-    return password
+    return pbkdf2_sha256.hash(password)
 
 def fromRegistrationModel(model: RegistrationModel):
-    return Account(id=model.id, name=model.name, email=model.email, phone=model.phone, address=model.address, activated=False, passhash=createPasshash(model.password))
+    return Account(name=model.name, email=model.email, phone=model.phone, address=model.address, activated=False, passhash=createPasshash(model.password), is_tenant=False, parent_tenant_id=None)
